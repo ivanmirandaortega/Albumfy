8418782489fae4f217b734a8aabb0f238b12658e
Ivan Miranda Ortega <imirandaortega.pd@gmail.com>
2022-05-24 14:33:18 -0700

    added the header to profile page

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { Grid } from 'semantic-ui-react'
+import PageHeader from '../../components/Header/Header'
+import PostGallery from '../../components/PostGallery/PostGallery'
 
-export default function ProfilePage() {
+export default function ProfilePage(props) {
 
     return (
-        <Grid textAlign='center'>
+        <Grid>
             <Grid.Row>
-                <div>Profile Page</div>
+                <Grid.Column>
+                    <PageHeader user={props.user} />
+                </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+                <Grid.Column>
+                    {/* <PostGallery /> */}
+                </Grid.Column>
             </Grid.Row>
         </Grid>
     )
