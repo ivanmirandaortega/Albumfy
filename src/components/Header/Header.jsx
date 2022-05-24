df43a6db8b9208c7dc94df7a5b6c7a0e2f94f754
Ivan Miranda Ortega <imirandaortega.pd@gmail.com>
2022-05-23 17:13:17 -0700

    added app name and react avatar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,8 +5,17 @@ import { Header, Segment, Image, Icon } from "semantic-ui-react";
 export default function PageHeader() {
 
     return (
-        <Segment>
-            <div>test</div>
+        <Segment clearing>
+            <Header floated="left">
+                <Link to='/'>App Name</Link>
+            </Header>
+            <Header floated="right">
+                <Link to=''>
+                    <Image src='https://react.semantic-ui.com/images/wireframe/square-image.png'
+                        avatar
+                    ></Image>
+                </Link>
+            </Header>
         </Segment>
     )
 }
