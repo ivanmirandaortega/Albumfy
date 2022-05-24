5fbb2354250ba875c676147ef27a0bd4ca502a83
Ivan Miranda Ortega <imirandaortega.pd@gmail.com>
2022-05-24 13:58:08 -0700

    added profile function in user controller and set up profile routes

---
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Grid } from 'semantic-ui-react'
+
+export default function ProfilePage() {
+
+    return (
+        <Grid>
+            <div>Profile Page</div>
+        </Grid>
+    )
+}
